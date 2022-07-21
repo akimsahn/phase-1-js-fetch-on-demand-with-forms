6ef760f9e28ecf8d5f00ed508b9e6e5eb77e552c
Allison Kim <akeem159@yahoo.com>
2022-07-21 14:00:17 -0400

    commit tree 38e1c0bb477fecfeafb32d9995d684d30a5db133

--- src/index.js
+++ src/index.js
@@ -1,5 +1,20 @@
 const init = () => {
-  
+  const inputForm = document.querySelector('form')
+  inputForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+    const input = e.target.searchByID.value
+    console.log(input)
+
+    fetch(`http://localhost:3000/movies/${input}`)
+    .then(resp => resp.json())
+    .then(data => {
+        const title = document.querySelector('section#movieDetails h4');
+        const summary = document.querySelector('section#movieDetails p');
+        console.log(data)
+        title.textContent = data.title;
+        summary.textContent = data.summary;
+    })
+  })
 }
 
 document.addEventListener('DOMContentLoaded', init);
